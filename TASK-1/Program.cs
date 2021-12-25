9e174d7881cedc057c46c441362ae905f2d5a954
Admin <Admin@WIN-4B40LAG9JOG>
2021-12-26 01:43:08 +0300

    Добавьте файлы проекта.

---
+++ TASK-1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TASK_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fullName = "Ф.И.О.";//объявил переменные
+            int age = 30;
+            string main = "Email";
+            double programm = 0.99;
+            double math = 76.4;
+            double physics = 64.2;
+            Console.WriteLine(" {0} \n Возраст: {1} \n {2}: \n Баллы по программированию: {3} \n Баллы по матиматике: {4} \n Баллы по физике: {5}",
+                              fullName, age, main, programm, math, physics);//форматированный вывод с переносом на новую строку для красоты.
+            Console.ReadKey();//закрыть программу нажатием на любую клавишу.
+
+
+        }
+    }
+}
---
+++ TASK-2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TASK_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double programm = 0.99;//объявил переменные,без них никак.
+            double math = 76.4;
+            double physics = 64.2;
+            Console.WriteLine(" Баллы по программированию:{0} \n Баллы по матиматике:{1} \n Баллы по физике:{2} ",
+                              programm, math, physics);//форматированный вывод.
+            Console.ReadKey();//последовательный вывод по нажатию клавиши(вот только символ остается пришлось переносить на следующую сторку для красоты)
+            double sum = programm + math + physics;//объявил переменную суммы баллов.
+            double average = sum / 3;//объявил переменную среднее арифметическое.
+            Console.WriteLine("\n Сумма баллов: {0} \n Среднее арифмитическое баллов: {1}", sum, average);//форматированный вывод.
+            Console.ReadKey();
+        }
+    }
+}
