42acd6529347f9ca8ffae33b886c5bec36cbb29f
AP91cs <apcb91@mail.ru>
2022-03-09 22:53:48 +0300

    LESSON

---
+++ TASK-10/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TASK_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение:");
+            string inputParse = Console.ReadLine();
+            static void ReversWords (string inputParse)
+           { 
+               string [] arr = SplitPharse(inputParse);
+                static string[] SplitPharse(string inputPharse)
+                {
+                    string[] arr = inputPharse.Split(' ');
+                    return arr;
+                }
+
+                Array.Reverse(arr);
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    Console.Write($"{arr[i]}" + " ");
+                }
+           }
+            ReversWords(inputParse);
+            Console.ReadKey();
+           
+        } 
+    }
+}
---
+++ TASK-9/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace TASK_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Введите предложение:");
+            string str = Console.ReadLine();
+            static string[] ArrStr(string str)
+            {
+                string[] arr = str.Split(' ');
+                return arr;
+            }
+            string [] arr = ArrStr(str);
+
+            static void NewStr (string [] arr)
+            {
+                foreach (var item in arr)
+                {
+                    Console.WriteLine($"{item}");
+                }
+                
+            }
+            NewStr(arr);
+            Console.ReadKey();
+
+        }
+    }
+}
